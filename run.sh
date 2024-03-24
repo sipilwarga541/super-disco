2be4f767f7cc2768b250f2d5a99f93ed90bab74b
sipilwarga541 <161419350+sipilwarga541@users.noreply.github.com>
2024-03-24 13:25:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-d7f650ef-2095-4343-b416-fe31645cd540/)
 
 
 echo "$curl_output" >> Update.tsx
